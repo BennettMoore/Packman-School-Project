eccb79eac5246cb202097abc587e21c40632dbc5
Bennett Moore <bwm7637@rit.edu>
2021-10-29 16:13:19 -0400

    Created empty Packman file

---
+++ packman.c
@@ -0,0 +1,21 @@
+// 
+// file: packman.c
+//
+// @author Bennett Moore bwm7637@rit.edu
+// // // // // // // // // // // // // // //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "HeapDT.h"
+#include "packman_utils.h"
+
+/*
+ * The main method
+ *
+ * @returns whether the program ran successfully or not
+ */
+int main(void){
+	return 0;
+}
