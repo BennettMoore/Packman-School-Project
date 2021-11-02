0509832eca56dc565df0cf7197e43f6d0aa52c83
Bennett Moore <bwm7637@rit.edu>
2021-11-02 13:00:52 -0400

    added empty pack_encode file and header file

---
+++ pack_encode.c
@@ -0,0 +1,17 @@
+//
+//file: pack_encode.c
+//
+//@author Bennett Moore bwm7637@rit.edu
+//
+// // // // // // // // // // // // // //
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "HeapDT.h"
+#include "packman_utils.h"
+#include "pack_encode.h"
+
+int main(void){ //Temporary main statement
+	return 0;
+}
---
+++ pack_encode.h
@@ -0,0 +1,17 @@
+//
+//file: pack_encode.h
+//
+//@author Bennett Moore bwm7637@rit.edu
+//
+// // // // // // // // // // // // // //
+
+#ifndef PACK_ENCODE_H
+#define PACK_ENCODE_H
+
+#include <stdio.h>
+#include "HeapDT.h"
+#include "packman_utils.h"
+
+
+
+#endif //PACK_ENCODE_H
