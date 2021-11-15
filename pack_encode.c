4c42b071a0a1f105bddadf35a56f8dbceaa56247
Bennett Moore <bwm7637@rit.edu>
2021-11-15 15:16:18 -0500

    Added preliminary Heap compare and print functions to encoder file

--- pack_encode.c
+++ pack_encode.c
@@ -14,6 +14,29 @@
 #include "packman_utils.h"
 #include "pack_encode.h"
 
+
+/**
+ * @brief Compares two Tree_nodes and returns the less frequent of the two
+ */
+int compare_syms(const void * lhs, const void * rhs){
+	Tree_node lhs_t = (Tree_node) lhs;
+	Tree_node rhs_t = (Tree_node) rhs;
+	return lhs_t < rhs_t;
+}
+
+/**
+ * @brief Prints a Tree_node to a specific stream 
+ */
+void print_sym(const void * item, FILE * outfp){
+	Tree_node item_t = (Tree_node) item;
+	if(item_t->sym != NUL){
+		fprintf(outfp, "%u:%c\n",item_t->freq, item_t->sym);
+	}
+	else{
+		fprintf(outfp, "%u:*\n", item_t->freq);
+	}
+}
+
 /**
  * @brief Find all symbols in the provided file
  */
@@ -22,10 +45,10 @@ uint * find_syms(FILE * input){
 	size_t nmemb = BUFSIZE/BITS_PER_BYTE;
 	size_t size = BITS_PER_BYTE;
 	size_t read_size = 0;
-	uint * syms = (uint *)calloc(BUFSIZE, sizeof(uint));
-	uchar * buffer = (uchar*)calloc(nmemb, sizeof(uchar));
+	uint * syms = (uint *)calloc(MAXSYM, sizeof(uint));
+	uchar * buffer = (uchar*)calloc(BUFSIZE, sizeof(uchar));
 
-	for(size_t i = 0; i < BUFSIZE; i++){
+	for(size_t i = 0; i < MAXSYM; i++){
 		syms[i] = 0;
 	}
 
@@ -44,6 +67,8 @@ uint * find_syms(FILE * input){
 	free(buffer);
 	return syms;
 }
+
+
 
 
 int main(void){ //Temporary main statement
