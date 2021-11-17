e56020bfff1d9c958775351b73482045c1909d16
Bennett Moore <bwm7637@rit.edu>
2021-11-17 15:44:34 -0500

    Created empty pack_decode files

---
+++ pack_decode.c
@@ -0,0 +1,17 @@
+//
+// file: pack_decode.c
+//
+// @author Bennett Moore bwm7637@rit.edu
+// // // // // // // // // // // // // //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include "packman_utils.h"
+#include "pack_decode.h"
+
+int main(void){ //Temporary main statement
+	return 0;
+}
---
+++ pack_decode.h
@@ -0,0 +1,15 @@
+//
+// file: pack_decode.h
+//
+// @author Bennett Moore bwm7637@rit.edu
+// // // // // // // // // // // // // //
+
+#ifndef PACK_DECODE_H
+#define PACK_DECODE_H
+
+#include <stdio.h>
+#include "packman_utils.h"
+
+
+
+#endif //PACK_DECODE_H
